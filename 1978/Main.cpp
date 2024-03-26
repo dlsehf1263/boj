08f296ede300c64f3070cb4f3b73209d43900654
MengTwo <dlsehf1263@naver.com>
2024-03-27 00:32:55 +0900

    Add files via upload

---
+++ 1978/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// 2024-03-27 00:25
+
+bool isPrime(int n) {
+    if (n == 1) return false;
+
+    for (int i = 2; i < n; ++i) {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int numbCount;
+    scanf("%d", &numbCount);
+
+    int primeCount = 0, numb;
+
+    for (int i = 0; i < numbCount; ++i) {
+        scanf("%d", &numb);
+        primeCount += isPrime(numb);
+    }
+
+    printf("%d", primeCount);
+
+    return 0;
+}
